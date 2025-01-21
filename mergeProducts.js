078f97eccbba155579c2505dd92eae7c0743bacb
e2e2ea <raymond.g@techguys.work>
2025-01-21 16:40:22 +0800

    Add

--- mergeProducts.js
+++ mergeProducts.js
@@ -136,8 +136,8 @@ const getData = async () => {
 (async () => {
   try {
     const externalApiUrl = 'https://tell-me-backend-dev.appelloproject.xyz/import-products';
-    const apiKey = 'x2M+ObTQi1pWce/Aeof0PRBK+cGht2RbUow4iwWFrA0=';
-    const a = fs.readFileSync('woolworths.json', 'utf-8')
+    const apiKey = '';
+    const a = fs.readFileSync('colesProducts.json', 'utf-8')
     const data = JSON.parse(JSON.stringify(a));
 
     const response = await axios.post(externalApiUrl, data, {
